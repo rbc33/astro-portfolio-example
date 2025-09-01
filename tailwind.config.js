09cd0da76387959a42713953931cbb80df06df1e
rbc33 <ricardobenthem@gmail.com>
2025-09-01 22:10:34 +0200

    Added Some icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				base: '16px',
+			},
+		},
 	},
 	plugins: [],
 }
